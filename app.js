d47263c085550be691ce6bfe04d047bc23c8c22b
Sec <qianqian1903@gmail.com>
2022-12-12 01:48:08 +0800

    ts

--- app.js
+++ app.js
@@ -3,86 +3,134 @@
 const currencyRates = [
 	{
 		name: "日幣",
-		rate: 1,
+		rate: 4.44,
+		id: Math.random(),
 	},
 	{
 		name: "美金",
-		rate: 2,
-	},
-	{
-		name: "澳幣",
-		rate: 3,
+		rate: 0.033,
+		id: Math.random(),
 	},
 	{
 		name: "韓幣",
-		rate: 4,
-	},
-	{
-		name: "印尼幣",
-		rate: 5,
+		rate: 43.7,
+		id: Math.random(),
 	},
 ];
 
 const App = () => {
-	const { useState, useRef } = React;
+	const { useState } = React;
 	const [rates, setRates] = useState(currencyRates);
-	const inputRef = useRef();
-	const [inputRefMemo, setInputRefMemo] = useState("");
-	const newCurrencyRateRef = useRef();
-	const newCurrencyNameRef = useRef();
+	const [exchangeInput, setExchangeInput] = useState(0);
+	const [currency, setCurrency] = useState({
+		name: "",
+		rate: "",
+	});
+	const [currencyMemo, setCurrencyMemo] = useState(0);
+	const [walletBalance, setWalletBalance] = useState(5000);
+	const [history, setHistory] = useState([]);
 
-	/*
-        NOTE: Calculate currency of each countries based on user input
-    */
+	const checkAmount = (value) => (value ? value.toFixed(2) : 0);
+
+	/*NOTE: Calculate currency of each countries based on user input*/
 	const calculateExchangeCurrency = () => {
+		setCurrencyMemo(exchangeInput);
+		if (exchangeInput <= 0) return;
 		setRates((prev) => {
-			const newRate = prev.map((item) => {
-				return { ...item, value: item.rate * parseInt(inputRef.current.value) };
+			const newRates = prev.map((item) => {
+				const amount = checkAmount(exchangeInput * item.rate);
+				return { ...item, amount, userExchangeInput: exchangeInput };
 			});
-			return newRate;
+			return newRates;
 		});
-		setInputRefMemo(inputRef.current.value);
+		setExchangeInput(0);
 	};
 
 	// NOTE: Add new currency when user click on submit button
 	const addNewCurrency = () => {
-		setRates((prev) => {
-			const newCurrency = [...prev];
-			const isValueExist = newCurrency.some((currency) => currency.value);
+		setRates((prevRates) => {
+			const amount = checkAmount(currency.rate * currencyMemo);
 			const currencyData = {
+				...currency,
+				amount,
 				id: Math.random(),
-				name: newCurrencyNameRef.current.value,
-				rate: newCurrencyRateRef.current.value,
-				value: isValueExist ? newCurrencyRateRef.current.value * inputRefMemo : null,
+				userExchangeInput: currencyMemo || 0,
 			};
-			newCurrency.unshift(currencyData);
-			return newCurrency;
+			return [currencyData, ...prevRates];
 		});
+		setCurrency({ name: "", rate: "" });
 	};
 
-	const isAddNewCurrencyDisabled = () => {
-		// const isValid = (val) => val.trim() !== "";
-		// return isValid(newCurrencyRateRef.current.value) && isValid(newCurrencyNameRef.current.value);
-		return newCurrencyRateRef.current.value.trim() !== "" && newCurrencyNameRef.current.value.trim() !== "";
+	const updateCurrency = (e) => {
+		const targetName = e.target.name;
+		setCurrency((prevCurrency) => ({ ...prevCurrency, [targetName]: e.target.value }));
 	};
 
-	console.log("diu lei");
+	const exchange = (exchangeData) => {
+		const balance = walletBalance - exchangeData.userExchangeInput;
+		if (balance < 0) {
+			alert("超出钱包金额");
+			return;
+		}
+		setWalletBalance(balance);
+		setHistory((prevHistory) => [exchangeData, ...prevHistory]);
+	};
+
+	console.log("hello world");
 
 	return (
 		<>
 			<h3>新增幣種</h3>
-			<input type="text" placeholder="幣種名稱" ref={newCurrencyNameRef} />
-			<input type="text" placeholder="匯率" ref={newCurrencyRateRef} />
-			<input type="button" value="新增幣種" onClick={addNewCurrency} />
+			<input type="text" name="name" placeholder="幣種名稱" value={currency.name} onChange={updateCurrency} />
+			<input type="text" name="rate" placeholder="匯率" value={currency.rate} onChange={updateCurrency} />
+			<input
+				type="button"
+				value="新增幣種"
+				onClick={addNewCurrency}
+				disabled={!currency.rate || !currency.name}
+			/>
+
 			<hr />
-			<input type="text" placeholder="台幣" ref={inputRef} />
+
+			<h3>您錢包還有 {walletBalance} 元</h3>
+			<p>請輸入您要換的台幣</p>
+
+			<input
+				type="text"
+				placeholder="台幣"
+				value={exchangeInput}
+				onChange={(e) => setExchangeInput(e.target.value)}
+			/>
 			<input type="button" value="計算" onClick={calculateExchangeCurrency} />
 			<p>可以換算</p>
 			<ul>
-				{rates.map((item, index) => {
-					const { name, value } = item;
-					return <li key={index}>{value ? `${name}: ${value}` : `${name}: `}</li>;
+				{rates.map((item) => {
+					const { name, amount, id } = item;
+					return (
+						<li key={id}>
+							{`${name}: ${amount || 0} 元`}
+							<button type="button" onClick={() => exchange(item)} disabled={amount > 0 ? false : true}>
+								兌換
+							</button>
+						</li>
+					);
 				})}
+			</ul>
+
+			<hr />
+			<h3>您的兌換記錄</h3>
+			<ul>
+				{history.length > 0
+					? history.map((item, index) => {
+							const { amount, name, userExchangeInput } = item;
+							return (
+								<li key={index}>
+									您用{userExchangeInput}元台幣, 兌換了{amount}
+									{name}
+								</li>
+							);
+					  })
+					: "目前没有任何记录."}
 			</ul>
 		</>
 	);
